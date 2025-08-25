386a321ca98c46d74742cceed8bb2c28ee605ed5
billy <billy.cook12345@gmail.com>
2025-08-25 15:01:49 -0400

    Forgot in previous commit

--- projects/ngx-cards/src/public-api.ts
+++ projects/ngx-cards/src/public-api.ts
@@ -2,10 +2,10 @@
  * Public API Surface of ngx-cards
  */
 
-export * from './lib/components/ngx-card/ngx-cards.service';
 export * from './lib/components/ngx-card/ngx-cards.component';
+export * from './lib/components/ngx-dice/ngx-dice.component';
 export * from './lib/models/card';
 export * from './lib/models/deck';
 export * from './lib/models/joker';
 export * from './lib/shared/enums';
-export const testString = 'Hello';
+export * from './lib/models/dice';
