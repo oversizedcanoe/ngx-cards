8e8152f2724a003b7014bcb051f79256e983226d
billy <billy.cook12345@gmail.com>
2025-08-25 14:59:25 -0400

    Add basic dice to library

--- projects/ngx-cards/src/lib/components/ngx-card/ngx-cards.component.ts
+++ projects/ngx-cards/src/lib/components/ngx-card/ngx-cards.component.ts
@@ -1,7 +1,6 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Deck } from '../../models/deck';
 import { Card } from '../../models/card';
-import { Subject } from 'rxjs';
 import { FaceState } from '../../shared/enums';
 
 @Component({
@@ -15,7 +14,7 @@ import { FaceState } from '../../shared/enums';
 export class NgxCardsComponent implements OnInit, OnDestroy {
 
   /**
-   * Required parameter to specify a specific card.
+   * Required parameter to specify an instance of a card.
    */
   @Input() card!: Card;
 
@@ -24,15 +23,15 @@ export class NgxCardsComponent implements OnInit, OnDestroy {
    */
   @Input() deckRef: Deck | undefined;
 
-  public fileName: string = '';
+  fileName: string = '';
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.updateImageSource();
-    this.card.flipped.subscribe((_) => {
-      this.updateImageSource();
+    this.updateImageSource(this.card.state);
+    this.card.flipped.subscribe((newFaceState) => {
+      this.updateImageSource(newFaceState);
     })
   }
 
@@ -40,15 +39,15 @@ export class NgxCardsComponent implements OnInit, OnDestroy {
     this.card.flipped.unsubscribe();
   }
 
-  updateImageSource() {
-    if (this.card.state == FaceState.FaceDown) {
+  updateImageSource(state: FaceState) {
+    if (state == FaceState.FaceDown) {
       this.fileName = `/assets/cards/R1.svg`;
     }
-    else if (this.card.state == FaceState.FaceUp) {
+    else if (state == FaceState.FaceUp) {
       this.fileName = `/assets/cards/${this.card.shorthandName()}.svg`;
     }
     else {
-      throw Error(`Unknown Card FaceState: ${this.card.state}`)
+      throw Error(`Unknown Card FaceState: ${state}`)
     }
   }
 }
---
+++ projects/ngx-cards/src/lib/components/ngx-dice/ngx-dice.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FaceState } from '../../shared/enums';
+import { Dice } from '../../models/dice';
+
+@Component({
+  selector: 'ngx-dice',
+  standalone: true,
+  imports: [],
+  templateUrl: './ngx-dice.component.html',
+  styleUrl: './ngx-dice.component.css'
+})
+
+export class NgxDiceComponent implements OnInit, OnDestroy {
+  /**
+   * Required parameter to specify an instance of a die.
+   */
+  @Input() dice!: Dice;
+
+  fileName: string = '';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.updateImageSource(this.dice.value);
+    this.dice.rolled.subscribe((newValue) => {
+      this.updateImageSource(newValue);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.dice.rolled.unsubscribe();
+  }
+
+  updateImageSource(newValue: number) {
+    this.fileName = `/assets/dice/d${newValue}.png`;
+  }
+}
